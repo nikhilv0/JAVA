9f27c50a8207ddea42c701091de6464aa642eeee
nikhilv0 <nikhilnikki6360@gmail.com>
2025-03-31 19:58:33 +0530

    Uploaded

--- EnumObjects/src/com/xworkz/enumObjects/Example.java
+++ EnumObjects/src/com/xworkz/enumObjects/Example.java
@@ -3,5 +3,6 @@ package com.xworkz.enumObjects;
 public class Example {
     public static void main(String[] args) {
         System.out.println(Match.TICKETS.getName());
+        System.out.println(Match.PLACE.getName());
     }
 }
