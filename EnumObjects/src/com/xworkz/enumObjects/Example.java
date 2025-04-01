ec4cc3ab2b84898392147e0ca3dbc371b4d9c46f
nikhilv0 <nikhilnikki6360@gmail.com>
2025-04-01 19:04:26 +0530

    Uploaded

--- EnumObjects/src/com/xworkz/enumObjects/Example.java
+++ EnumObjects/src/com/xworkz/enumObjects/Example.java
@@ -1,8 +1,20 @@
 package com.xworkz.enumObjects;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Example {
     public static void main(String[] args) {
         System.out.println(Match.TICKETS.getName());
         System.out.println(Match.PLACE.getName());
+
+//        InbuiltClass inbuiltClass=new InbuiltClass();
+//        inbuiltClass.setLocalDate("ygytu7");
+
+        System.out.println(LocalDate.of(2025,5,21));
+        System.out.println(LocalDate.now());
+        System.out.println(LocalDateTime.now());
+        System.out.println(LocalDateTime.of(2026,5,31,12,20,12));
+
     }
 }
---
+++ EnumObjects/src/com/xworkz/enumObjects/InbuiltClass.java
@@ -0,0 +1,26 @@
+package com.xworkz.enumObjects;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class InbuiltClass {
+    private LocalDate localDate;
+    private LocalDateTime localDateTime;
+
+//    public LocalDate getLocalDate() {
+//        return localDate;
+//    }
+//
+//    public void setLocalDate(LocalDate localDate) {
+//        this.localDate = localDate;
+//    }
+//
+//    public LocalDateTime getLocalDateTime() {
+//        return localDateTime;
+//    }
+//
+//    public void setLocalDateTime(LocalDateTime localDateTime) {
+//        this.localDateTime = localDateTime;
+//    }
+}
+
