d2bbb8b3f9b91a0d8322a0662e9435d78948e08d
nikhilv0 <nikhilnikki6360@gmail.com>
2025-08-01 10:28:26 +0530

    JPA

---
+++ JPA/src/main/java/com/xworkz/kingdom/KingdomEntity.java
@@ -0,0 +1,27 @@
+package com.xworkz.kingdom;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Table(name = "kingdom_table")
+public class KingdomEntity {
+
+    @Id
+    private int id;
+
+    @Column(name = "king_name")
+    private String kingName;
+
+    @Column(name = "queen_name")
+    private String queenName;
+}
---
+++ JPA/src/main/java/com/xworkz/kingdom/KingdomMain.java
@@ -0,0 +1,28 @@
+package com.xworkz.kingdom;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class KingdomMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf= Persistence.createEntityManagerFactory("kingdom");
+        EntityManager em=emf.createEntityManager();
+        EntityTransaction et=em.getTransaction();
+        et.begin();
+
+        List<KingdomEntity> kingdom=new ArrayList<>(Arrays.asList(new KingdomEntity(1,"nikki","nikitha"),new KingdomEntity(2,"aryan","megha")));
+        for (KingdomEntity kingdom1:kingdom){
+        em.persist(kingdom1);
+        }
+
+//        KingdomEntity kingdomEntity=em.find(k.class);
+        et.commit();
+        em.close();
+
+    }
+}
