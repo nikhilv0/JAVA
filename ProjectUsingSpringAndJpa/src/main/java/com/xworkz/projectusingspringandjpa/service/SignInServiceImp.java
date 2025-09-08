ad0285bc41ef936b111d2feed682f6f1abcbdd76
nikhilv0 <nikhilnikki6360@gmail.com>
2025-09-08 23:30:41 +0530

    Project Using Spring and JPA

--- ProjectUsingSpringAndJpa/src/main/java/com/xworkz/projectusingspringandjpa/service/SignInServiceImp.java
+++ ProjectUsingSpringAndJpa/src/main/java/com/xworkz/projectusingspringandjpa/service/SignInServiceImp.java
@@ -5,15 +5,15 @@ import com.xworkz.projectusingspringandjpa.entity.SignInEntity;
 import com.xworkz.projectusingspringandjpa.entity.SignUpEntity;
 import com.xworkz.projectusingspringandjpa.repository.SignInRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpSession;
 import java.security.SecureRandom;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 
 @Service
@@ -23,6 +23,8 @@ public class SignInServiceImp implements SignInService {
 
     @Autowired
     JavaMailSender javaMailSender;
+
+    private HttpSession httpSession;
 
     private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
@@ -35,32 +37,42 @@ public class SignInServiceImp implements SignInService {
         }
 
         SecureRandom random = new SecureRandom();
-        int otp = 100000 + random.nextInt(900000);
+        int otp1 = 100000 + random.nextInt(900000);
+        LocalDateTime expiryTime = LocalDateTime.now().plusMinutes(2);
+        String otp = String.valueOf(otp1);
         System.out.println("Your OTP is: " + otp);
 
         sendOtpToEmail(signInDTO, otp);
 
-        if (passwordEncoder.matches(signInDTO.getPassword(), storedUser.getPassword())) {
-            SignInEntity signInEntity = new SignInEntity();
-            signInEntity.setEmail(signInDTO.getEmail());
-            signInEntity.setPassword(storedUser.getPassword());
-            signInEntity.setVisitedAt(new Timestamp(System.currentTimeMillis()));
-            System.out.println(signInRepository.login(signInEntity));
-            return "Login successful!";
+        String inputOtp = (String) httpSession.getAttribute("");
+        if (otp.equals(inputOtp) && (LocalDateTime.now().isAfter(expiryTime))) {
+//            Thread.sleep(1000);
+
+            if (passwordEncoder.matches(signInDTO.getPassword(), storedUser.getPassword())) {
+                SignInEntity signInEntity = new SignInEntity();
+                signInEntity.setEmail(signInDTO.getEmail());
+                signInEntity.setPassword(storedUser.getPassword());
+                signInEntity.setVisitedAt(new Timestamp(System.currentTimeMillis()));
+                System.out.println(signInRepository.login(signInEntity));
+                return "Login successful!";
+            } else {
+                return "Invalid password!";
+            }
+
         }
-        else {
-            return "Invalid password!";
-        }
+        return "Invalid Otp";
 
     }
 
-    private void sendOtpToEmail(SignInDTO signInDTO,int otp){
-        SimpleMailMessage message=new SimpleMailMessage();
+    private void sendOtpToEmail(SignInDTO signInDTO, String otp) {
+        SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("nikhilnikki6360@gmail.com");
         message.setTo(signInDTO.getEmail());
         message.setSubject("Your OTP for Login Verification");
-        message.setText("Your One-Time Password (OTP) is: " + otp);
+        message.setText("Your One-Time Password (OTP) is: " + otp + "\n"
+                + "This OTP is valid for 2 minutes.");
 
         javaMailSender.send(message);
     }
 }
+
