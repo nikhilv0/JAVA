a4d7bfbcca9c19cf4e295a7a534376a99d4531c4
nikhilv0 <nikhilnikki6360@gmail.com>
2025-09-18 21:09:35 +0530

    Project Using Spring and JPA

--- ProjectUsingSpringAndJpa/src/main/java/com/xworkz/projectusingspringandjpa/configuration/AppConfiguration.java
+++ ProjectUsingSpringAndJpa/src/main/java/com/xworkz/projectusingspringandjpa/configuration/AppConfiguration.java
@@ -3,18 +3,27 @@ package com.xworkz.projectusingspringandjpa.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
 @EnableWebMvc
+@EnableTransactionManagement
 @ComponentScan(basePackages = "com.xworkz")
 public class AppConfiguration implements WebMvcConfigurer {
 
@@ -22,9 +31,10 @@ public class AppConfiguration implements WebMvcConfigurer {
         System.out.println("AppConfiguration Constructor");
     }
 
+
     @Bean
-    public InternalResourceViewResolver viewResolver(){
-        return new InternalResourceViewResolver("/",".jsp");
+    public InternalResourceViewResolver viewResolver() {
+        return new InternalResourceViewResolver("/", ".jsp");
     }
 
     @Override
@@ -32,10 +42,44 @@ public class AppConfiguration implements WebMvcConfigurer {
         configurer.enable();
     }
 
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/Spring_Project");
+        dataSource.setUsername("root");
+        dataSource.setPassword("Nikki6360");
+        return dataSource;
+    }
+
+    // ---------- EntityManagerFactory (replaces persistence.xml) ----------
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("com.xworkz.projectusingspringandjpa.entity"); // adjust package
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setShowSql(true);
+        vendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+
+        em.setJpaVendorAdapter(vendorAdapter);
+
+        Properties props = new Properties();
+        props.setProperty("hibernate.hbm2ddl.auto", "update");
+        props.setProperty("hibernate.format_sql", "true");
+        em.setJpaProperties(props);
+
+        return em;
+    }
+
+    // ---------- Transaction Manager ----------
 //    @Bean
-//    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-//
+//    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+//        return new JpaTransactionManager(emf);
 //    }
+
 
     @Bean
     public JavaMailSender getJavaMailSender() {
